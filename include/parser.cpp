ba28005e71c4e3811b047a144b60b2e5c859962d
MiguelSantanaDaCosta <miguelsantanadacosta@gmail.com>
2025-07-12 01:12:59 -0300

    iniciando a implementação do parser

--- include/parser.cpp
+++ include/parser.cpp
@@ -1,21 +1,139 @@
-// include/parser.h
-#pragma once
-
-#include "ast.h"
+#include <vector>
+#include <memory>
+#include <stdexcept>
+#include <functional>
+#include <unordered_map>
 #include "lexer.h"
 
-class Parser {
+//===============================================
+//FORWARD DECLARATIONS PARA  OS NÓS DA AST
+//================================================
+
+class Program;
+class ClassDec1;
+class FunctionDec1;
+class VarDec1;
+class BlockStmt;
+class Expr;
+class AssignExpr;
+class BinaryExpr;
+class LogicalExpr;
+class CallExpr;
+class VariableExpr;
+class LiteralExpr;
+class GroupingExpr;
+class UnaryExpt;
+class GetExpr;
+class SetExpr;
+class ThisExpr;
+class SuperExpr;
+
+//=============================
+//DEFINIÇÕES DE TIPOS 
+//=============================
+using ExprPtr = std::unique_ptr<Expr>;
+using StmtPtr = std::unique_ptr< class Stmt>;
+using Dec1Ptr = std::unique_ptr<class Declaration>;
+
+//=========================
+//CLASSE PARA DECLARAÇÕES
+//=========================
+class Declaration {
 public:
-    explicit Parser(const std::vector<Token>& tokens);
-    std::unique_ptr<Program> parse();
-    
-private:
-    const std::vector<Token>& tokens_;
-    size_t current_ = 0;
-    
-    // Métodos de parsing recursivo
-    std::unique_ptr<ClassDecl> parseClassDeclaration();
-    std::unique_ptr<FunctionDecl> parseFunction();
-    std::unique_ptr<Statement> parseStatement();
-    // ... outros métodos de parsing
+   virtual ~Declaration() = default;
 };
+
+//====================================
+//CLASSE PARA COMANDOS (STATEMENTS)
+//====================================
+class Stmt {
+  public:
+    virtual ~Stmt() = default;
+};
+
+//===================================
+//CLASSE PARA EXPRESSÕES
+//===================================
+class Expr {
+  public:
+    virtual ~Expr() = default;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
